9b5a7bedb6ff41890583e1e4cc511abada941046
OscarDRR <118695303+OscarDRR@users.noreply.github.com>
2024-04-29 12:34:21 -0600

    Add files via upload

---
+++ Entidad_6.vhd
@@ -0,0 +1,15 @@
+-- Bibliotecas
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+--Entidad
+
+entity Entidad_6 is
+
+--Declaracion de puertos
+
+port(
+	--Entradas A,B y C
+	Entradas: in std_LOGIC_1164
+	
+)
